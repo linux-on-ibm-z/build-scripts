ce7da0dd8718e1e7149dfeff1d787f2338f3dc5f
Sudip Roy <123369437+sudip-ibm@users.noreply.github.com>
2026-05-19 17:33:14 +0530

    Xmlsec: Update maintainer name (#78)
    * Update maintainer name in build_info.json
    
    * Update maintainer information in xmlsec_ubi_9_3.sh

--- x/xmlsec/xmlsec_ubi_9_3.sh
+++ x/xmlsec/xmlsec_ubi_9_3.sh
@@ -8,7 +8,7 @@
 # Language       : c
 # Travis-Check   : True
 # Script License : Apache License, Version 2 or later
-# Maintainer     : Yash Ratawa <Yash.Ratawa@ibm.com>
+# Maintainer     : Sankalp <Sankalp@ibm.com>
 #
 # Disclaimer: This script has been tested in root mode on given
 # ==========  platform using the mentioned version of the package.
