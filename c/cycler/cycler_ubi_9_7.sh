c4110ae411d5ec86745418d0055b37aa07a0eafc
Keerthana G R <Keerthana.G.R@ibm.com>
2026-06-10 12:23:27 +0530

    cycler v0.12.1

---
+++ c/cycler/cycler_ubi_9_7.sh
@@ -0,0 +1,56 @@
+#!/bin/bash -e
+# ----------------------------------------------------------------------------
+#
+# Package       : cycler
+# Version       : v0.12.1
+# Source repo   : https://github.com/matplotlib/cycler
+# Tested on     : UBI:9.7
+# Language      : Python
+# Ci-Check      : True
+# Script License: Apache License, Version 2 or later
+# Maintainer    : Keerthana GR <Keerthana.G.R@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# ----------------------------------------------------------------------------
+
+# Variables
+PACKAGE_NAME=cycler
+PACKAGE_VERSION=${1:-v0.12.1}
+PACKAGE_URL=https://github.com/matplotlib/cycler
+
+# Install dependencies
+yum install -y python3 python3-pip python3-devel gcc git
+
+# Clone repository
+git clone $PACKAGE_URL
+cd $PACKAGE_NAME
+git checkout $PACKAGE_VERSION
+
+# Install pytest for testing
+pip3 install pytest
+
+# Install package
+if ! python3 -m pip install . ; then
+    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+# Run tests
+if ! pytest; then
+    echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
