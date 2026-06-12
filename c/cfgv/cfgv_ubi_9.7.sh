a926caa73e12309918996f40267d060eda63e8b3
Viddya K <viddya.k@ibm.com>
2026-06-12 16:28:49 +0530

    Updating Travis-check to Ci-check

--- c/cfgv/cfgv_ubi_9.7.sh
+++ c/cfgv/cfgv_ubi_9.7.sh
@@ -6,7 +6,7 @@
 # Source repo   : https://github.com/asottile/cfgv.git
 # Tested on     : UBI:9.7
 # Language      : Python
-# Travis-Check  : True
+# Ci-Check      : True
 # Script License: Apache License, Version 2 or later
 # Maintainer    : Viddya <viddya.k@ibm.com>
 #
